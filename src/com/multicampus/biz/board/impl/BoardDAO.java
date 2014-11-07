36cc42dfca3091bd92f3c33682fc618c498058d9
ez2sugul <seunghoon100@gmail.com>
2014-11-07 09:39:21 +0900

    init

---
+++ src/com/multicampus/biz/board/impl/BoardDAO.java
@@ -0,0 +1,45 @@
+package com.multicampus.biz.board.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.multicampus.biz.board.vo.BoardVO;
+
+public class BoardDAO {
+
+	public void insertBoard(BoardVO vo) throws SQLException{		
+		
+	}	
+	
+	public void updateBoard(BoardVO vo) throws SQLException{		
+		
+	}	
+	
+	public void deleteBoard(BoardVO vo) throws SQLException{		
+		
+	}	
+	
+	public BoardVO getBoard(BoardVO vo) throws SQLException{		
+		return null;
+	}	
+	
+	public List<BoardVO> getBoardList(BoardVO vo) throws SQLException{		
+		return null;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
